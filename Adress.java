ba31649a4110c123fecff4b84dbc6d0cfbf2f779
Yakup SENATES <76456346+Yakup-SENATES@users.noreply.github.com>
2021-05-26 18:52:19 +0300

    Add files via upload

---
+++ Adress.java
@@ -0,0 +1,37 @@
+package com.hibernate.demo;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Adress {
+	
+	private String street , state , pincode ;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+
+	
+	
+	
+}
---
+++ City.java
@@ -0,0 +1,91 @@
+package com.hibernate.demo;
+
+import javax.persistence.*;
+
+@Entity // veritabaný nesnesi olduðunu belirtir
+@Table(name = "city")
+public class City {
+
+	@Id	 @Column(name = "ID") 
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(name = "Population")
+	private int population;
+
+	@Column(name = "Name")
+	private String name;
+
+	@Column(name = "CountryCode")
+	private String countryCode;
+
+	@Column(name = "District")
+	private String district;
+	
+	
+	@Embedded
+	private Adress adress;
+	
+	
+
+	public Adress getAdress() {
+		return adress;
+	}
+
+	public void setAdress(Adress adress) {
+		this.adress = adress;
+	}
+
+	public City() {
+	}
+
+	public City(int id, int population, String name, String countryCode, String district) {
+		super();
+		this.id = id;
+		this.population = population;
+		this.name = name;
+		this.countryCode = countryCode;
+		this.district = district;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String disrtrict) {
+		this.district = disrtrict;
+	}
+
+}
